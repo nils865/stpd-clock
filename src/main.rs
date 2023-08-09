5bb0c40a86667e1aa627e1baacc457268acc22a4
nils865 <nils.staas0806@gmail.com>
2023-08-09 11:50:27 +0200

    string to big string

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use nums::{display_zero, display_space};
+use nums::display_number;
 
 mod nums;
 
 fn main() {
     let mut display: [String; 5] = [String::from(""), String::from(""), String::from(""), String::from(""), String::from("")];
 
-    display_zero(&mut display);
-    display_space(&mut display);
-    display_zero(&mut display);
+    // display_zero(&mut display);
+    // display_space(&mut display);
+    // display_zero(&mut display);
+
+    display_number(&mut display, "1001");
 
     for e in &display {
         println!("{}", e);
--- src/nums.rs
+++ src/nums.rs
@@ -1,8 +1,28 @@
-pub fn append_arr(arr: &mut [String; 5], append: &str, pos: usize) {
+pub fn display_number(display: &mut [String; 5], num: &str) {
+    let mut counter = 0;
+
+    for c in num.chars() {
+
+        match c {
+            '0' => display_zero(display),
+            '1' => display_one(display),
+            _ => (),
+        }
+
+        if counter == num.chars().count() {
+            break;
+        }
+
+        display_space(display);
+        counter += 1;
+    }
+}
+
+fn append_arr(arr: &mut [String; 5], append: &str, pos: usize) {
     arr[pos] = String::from(arr[pos].clone() + append);
 }
 
-pub fn display_space(display: &mut [String; 5]) {
+fn display_space(display: &mut [String; 5]) {
     append_arr(display, "  ", 0);
     append_arr(display, "  ", 1);
     append_arr(display, "  ", 2);
@@ -10,10 +30,18 @@ pub fn display_space(display: &mut [String; 5]) {
     append_arr(display, "  ", 4);
 }
 
-pub fn display_zero(display: &mut [String; 5]) {
+fn display_zero(display: &mut [String; 5]) {
     append_arr(display, "██████", 0);
     append_arr(display, "██  ██", 1);
     append_arr(display, "██  ██", 2);
     append_arr(display, "██  ██", 3);
+    append_arr(display, "██████", 4);
+}
+
+fn display_one(display: &mut [String; 5]) {
+    append_arr(display, "  ██  ", 0);
+    append_arr(display, "████  ", 1);
+    append_arr(display, "  ██  ", 2);
+    append_arr(display, "  ██  ", 3);
     append_arr(display, "██████", 4);
 }
