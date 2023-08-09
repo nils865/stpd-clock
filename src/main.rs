0501c683c4f73e4652c200e98c66cc03956496db
nils865 <nils.staas0806@gmail.com>
2023-08-09 11:57:33 +0200

    added more numbers

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     // display_space(&mut display);
     // display_zero(&mut display);
 
-    display_number(&mut display, "1001");
+    display_number(&mut display, "01:23:45:67:89");
 
     for e in &display {
         println!("{}", e);
--- src/nums.rs
+++ src/nums.rs
@@ -4,12 +4,21 @@ pub fn display_number(display: &mut [String; 5], num: &str) {
     for c in num.chars() {
 
         match c {
+            ':' => display_colon(display),
             '0' => display_zero(display),
             '1' => display_one(display),
+            '2' => display_two(display),
+            '3' => display_three(display),
+            '4' => display_four(display),
+            '5' => display_five(display),
+            '6' => display_six(display),
+            '7' => display_seven(display),
+            '8' => display_eight(display),
+            '9' => display_nine(display),
             _ => (),
         }
 
-        if counter == num.chars().count() {
+        if counter + 1 == num.chars().count() {
             break;
         }
 
@@ -30,6 +39,14 @@ fn display_space(display: &mut [String; 5]) {
     append_arr(display, "  ", 4);
 }
 
+fn display_colon(display: &mut [String; 5]) {
+    append_arr(display, "  ", 0);
+    append_arr(display, "██", 1);
+    append_arr(display, "  ", 2);
+    append_arr(display, "██", 3);
+    append_arr(display, "  ", 4);
+}
+
 fn display_zero(display: &mut [String; 5]) {
     append_arr(display, "██████", 0);
     append_arr(display, "██  ██", 1);
@@ -44,4 +61,68 @@ fn display_one(display: &mut [String; 5]) {
     append_arr(display, "  ██  ", 2);
     append_arr(display, "  ██  ", 3);
     append_arr(display, "██████", 4);
+}
+
+fn display_two(display: &mut [String; 5]) {
+    append_arr(display, "██████", 0);
+    append_arr(display, "    ██", 1);
+    append_arr(display, "  ██  ", 2);
+    append_arr(display, "██    ", 3);
+    append_arr(display, "██████", 4);
+}
+
+fn display_three(display: &mut [String; 5]) {
+    append_arr(display, "██████", 0);
+    append_arr(display, "    ██", 1);
+    append_arr(display, "  ████", 2);
+    append_arr(display, "    ██", 3);
+    append_arr(display, "██████", 4);
+}
+
+fn display_four(display: &mut [String; 5]) {
+    append_arr(display, "██    ", 0);
+    append_arr(display, "██  ██", 1);
+    append_arr(display, "██████", 2);
+    append_arr(display, "    ██", 3);
+    append_arr(display, "    ██", 4);
+}
+
+fn display_five(display: &mut [String; 5]) {
+    append_arr(display, "██████", 0);
+    append_arr(display, "██    ", 1);
+    append_arr(display, "██████", 2);
+    append_arr(display, "    ██", 3);
+    append_arr(display, "██████", 4);
+}
+
+fn display_six(display: &mut [String; 5]) {
+    append_arr(display, "██    ", 0);
+    append_arr(display, "██    ", 1);
+    append_arr(display, "██████", 2);
+    append_arr(display, "██  ██", 3);
+    append_arr(display, "██████", 4);
+}
+
+fn display_seven(display: &mut [String; 5]) {
+    append_arr(display, "██████", 0);
+    append_arr(display, "    ██", 1);
+    append_arr(display, "    ██", 2);
+    append_arr(display, "    ██", 3);
+    append_arr(display, "    ██", 4);
+}
+
+fn display_eight(display: &mut [String; 5]) {
+    append_arr(display, "██████", 0);
+    append_arr(display, "██  ██", 1);
+    append_arr(display, "██████", 2);
+    append_arr(display, "██  ██", 3);
+    append_arr(display, "██████", 4);
+}
+
+fn display_nine(display: &mut [String; 5]) {
+    append_arr(display, "██████", 0);
+    append_arr(display, "██  ██", 1);
+    append_arr(display, "██████", 2);
+    append_arr(display, "    ██", 3);
+    append_arr(display, "    ██", 4);
 }
