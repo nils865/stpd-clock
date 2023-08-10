fe42e0a5a4d867d9f29e9260dcf7c0fef42cb8d7
nils865 <nils.staas0806@gmail.com>
2023-08-10 10:18:45 +0200

    Centered the Clock

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,71 @@
 use nums::display_number;
-use utils::formatted_time;
+use term_size::dimensions;
+use utils::{formatted_time, center_text};
 
 mod nums;
 mod utils;
+
+fn output(terminal_width: &usize, terminal_height: &usize, time: &mut String, display: &mut [String; 5]) {
+    *time = formatted_time();
+
+    display_number(display, &time);
+    
+    let text = center_text(display, *terminal_width, *terminal_height);
+
+
+    print!("{}", text);
+
+    print!("\x1B[{}A \x1B[G", terminal_height);
+}
 
 fn main() {
     let mut display: [String; 5];
 
     let mut time: String = formatted_time();
 
+    let mut terminal_width: usize = 0;
+    let mut terminal_height: usize = 0;
+
+    let mut new_terminal_width: usize = 0;
+    let mut new_terminal_height: usize = 0;
+
     loop {
         display = [String::from(""), String::from(""), String::from(""), String::from(""), String::from("")];
 
-        if time != formatted_time() {
-            time = formatted_time();
-
-            display_number(&mut display, &time);
-            
-            for e in &display {
-                println!("{}", e);
-            }
-    
-            print!("\x1B[5A \x1B[G");
+        if let Some((w, h)) = dimensions() {
+            new_terminal_width = w;
+            new_terminal_height = h;
         }
+
+        if new_terminal_width != terminal_width || new_terminal_height != terminal_height {
+            terminal_height = new_terminal_height;
+            terminal_width = new_terminal_width;
+
+            print!("\x1B[2J");
+            output(&terminal_width, &terminal_height, &mut time, &mut display);
+        } else if time != formatted_time() {
+            output(&terminal_width, &terminal_height, &mut time, &mut display);
+        }
+
+        // if time != formatted_time() || new_terminal_width != terminal_width || new_terminal_height != terminal_height {
+        //     terminal_height = new_terminal_height;
+        //     terminal_width = new_terminal_width;
+            
+        //     time = formatted_time();
+
+            
+        //     display_number(&mut display, &time);
+            
+        //     let text = center_text(&mut display, terminal_width, terminal_height);
+
+        //     // for e in &display {
+        //     //     println!("{}", e);
+        //     // }
+
+        //     print!("{}", text);
+    
+        //     print!("\x1B[{}A \x1B[G", terminal_height);
+        // }
     }
 
 }
--- src/utils.rs
+++ src/utils.rs
@@ -9,3 +9,38 @@ pub fn formatted_time() -> String {
 
     return time;
 }
+
+pub fn center_text(arr: &mut [String; 5], width: usize, height: usize) -> String {
+    // let mut centered_lines: [String; 5];
+
+    for i in 0..arr.len() {
+        let padding_width: usize = ((((width - arr[i].chars().count()) / 2) as f32).floor()) as usize;
+        let mut padding_str = String::new();
+
+        for _ in 0..padding_width {
+            padding_str = String::from(padding_str + " ");
+        }
+
+        // println!("{} {} {} {}\n", i, width, arr[i].chars().count(), padding_width);
+
+        arr[i] = String::from(padding_str + &arr[i].clone());
+    }
+
+    let top_padding_size: usize = (((height - arr.len()) / 2) as f32).floor() as usize;
+    let bottom_padding_size: usize = height - arr.len() - top_padding_size as usize;
+
+    let mut top_padding = String::new();
+    let mut bottom_padding = String::new();
+
+    for _ in 0..top_padding_size {
+        top_padding = String::from(top_padding + "\n");
+    }
+
+    for _ in 0..bottom_padding_size {
+        bottom_padding = String::from(bottom_padding + "\n");
+    }
+
+    // println!("{} {} {} {}", top_padding_size, bottom_padding_size, arr.len(), height);
+
+    return String::from(top_padding + &arr.join("\n") + &bottom_padding);
+}
